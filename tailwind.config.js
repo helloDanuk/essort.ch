ba7c02dc53c13167e2f1d8e9eb143fbee36ef344
helloDanuk <hello@danielkaufmann.ch>
2022-04-25 16:07:30 +0200

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,12 @@ module.exports = {
     './resources/**/*.vue',
     './content/**/*.md'
   ],
+  // safelist: ['class-name', {or pattern:… möglich}],
   theme: {
     extend: {},
   },
   plugins: [
+    require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
   ],
 }
