1ce33a56a3b04357a781a8c824c79ebd1fa92362
helloDanuk <hello@danielkaufmann.ch>
2022-04-25 16:07:14 +0200

    borwsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -39,3 +39,36 @@ if (mix.inProduction()) {
 //     require('tailwindcss/nesting'),
 //     require('tailwindcss'),
 // ])
+
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Browser Sync
+ |--------------------------------------------------------------------------
+ |
+ | https://laravel-mix.com/docs/main/browsersync
+ |
+ | ist über essort.test:3000 erreichbar
+ |
+ */
+
+mix.browserSync({
+    // proxy ist für valet nötig
+    proxy: 'essort.test',
+    // host und open ermöglichen, die Seite über essort.test:3000 zu öffnen -> ohne die beiden wird localhost:3000 geöffnet
+    host: 'essort.test',
+    open: 'external',
+    // Inject CSS changes -> ist true bei default
+    // injectChanges: true,
+    // Don't show any notifications in the browser.
+    notify: false,
+    browser: 'google chrome',
+    // ghostMode ist eine Empfehlung von Sizzy -> https://dev.to/superterrific/how-to-turn-off-browsersync-ghostmode-in-eleventy-2ofh
+    ghostMode: false,
+    files: [
+        './content/**/*.(yaml|md)',
+        './resources/views/**/*.html',
+        './public/**/*.(css|js)',
+    ]
+});
